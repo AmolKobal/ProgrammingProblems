210c9a124ff7a4e62b1fd86df14db31ccdcb251c
Amol Kobal <amol_kobal@rediffmail.com>
2019-10-14 23:46:26 +0530

    Sequential list operations

---
+++ SequentialList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgrammingProblems
+{
+    /// <summary>
+    /// Check if given list is subset of another list.
+    /// E.g. Set = (1, 3, 5, 7), Subset = (1, 3)
+    /// Returns true if all elementsof Subset is available in Set.
+    /// </summary>
+    class SequentialList
+    {
+        int[] set;
+
+        public SequentialList(int[] list)
+        {
+            set = list;
+        }
+
+        public bool IsSubset(int[] subset)
+        {
+            foreach (int value in subset)
+            {
+                bool found = false;
+                foreach (int item in set)
+                {
+                    if (value.Equals(item))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static string Print(int[] list)
+        {
+            string output = "";
+            output = "[ ";
+            foreach (int item in list)
+                output += item + " ";
+            output += "]";
+            return output;
+        }
+    }
+
+}
