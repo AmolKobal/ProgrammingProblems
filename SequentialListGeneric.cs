9ede1ecaa00eb49889fb3389c8499612ed6e7ef5
Amol Kobal <amol_kobal@rediffmail.com>
2019-10-14 23:53:40 +0530

    Generic Sequential list implementation and test

---
+++ SequentialListGeneric.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgrammingProblems
+{
+    class SequentialList<T>
+    {
+        T[] set;
+
+        public SequentialList(T[] list)
+        {
+            set = list;
+        }
+
+        public bool IsSubset(T[] subset)
+        {
+            foreach (T value in subset)
+            {
+                bool found = false;
+                foreach (T item in set)
+                {
+                    if (value.Equals(item))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (found == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static string Print(T[] list)
+        {
+            string output = "";
+            output = "[ ";
+            foreach (T item in list)
+                output += item + " ";
+            output += "]";
+            return output;
+        }
+    }
+}
---
+++ Test/SequentialListTest.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+namespace ProgrammingProblems.Test
+{
+    class SequentialListTest
+    {
+        public static void Run()
+        {
+            int[] a = { 1, 3, 5, 7, 9, 10, 11, 13 };
+            int[] b = { 5, 7, 13 };
+
+            SequentialList list = new SequentialList(a);
+
+            WriteLine($"{SequentialList.Print(b)} is subset of {SequentialList.Print(a)} = {list.IsSubset(b)}");
+
+            int[] c = { 1, 3, 5, 7 };
+            int[] d = { 1, 4 };
+
+            list = new SequentialList(c);
+
+            WriteLine($"{SequentialList.Print(d)} is subset of {SequentialList.Print(c)} = {list.IsSubset(d)}");
+
+            string[] str1 = { "one", "three", "five", "seven" };
+            string[] str2 = { "one", "four" };
+
+            SequentialList<string> glist = new SequentialList<string>(str1);
+
+            WriteLine($"{SequentialList<string>.Print(str2)} is subset of {SequentialList<string>.Print(str1)} = {glist.IsSubset(str2)}");
+
+        }
+    }
+}
