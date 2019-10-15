268b71175ec30e30c3ec637b5b4cddea429635aa
Amol Kobal <amol_kobal@rediffmail.com>
2019-10-15 23:52:57 +0530

    Check Number X is sum of 2 numbers in List

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using static System.Console;
+using ProgrammingProblems.Test;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListOperationsTest.Run();
+
+            SequentialListTest.Run();
+            ReadLine();
+            return;
+
+        }
+    }
+}
