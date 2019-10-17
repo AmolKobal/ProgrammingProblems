1519c22601d48de12cc2a889b39a8ffed663504a
Amol Kobal <amol_kobal@rediffmail.com>
2019-10-17 22:42:23 +0530

    Used extension of Print

--- Test/SequentialListTest.cs
+++ Test/SequentialListTest.cs
@@ -11,22 +11,27 @@ namespace ProgrammingProblems.Test
 
             SequentialList list = new SequentialList(a);
 
-            WriteLine($"{SequentialList.Print(b)} is subset of {SequentialList.Print(a)} = {list.IsSubset(b)}");
+            WriteLine($"{b.Print()} is subset of {a.Print()} = {list.IsSubset(b)}");
+
+            WriteLine(new string('═', 100));
 
             int[] c = { 1, 3, 5, 7 };
             int[] d = { 1, 4 };
 
             list = new SequentialList(c);
 
-            WriteLine($"{SequentialList.Print(d)} is subset of {SequentialList.Print(c)} = {list.IsSubset(d)}");
+            WriteLine($"{d.Print()} is subset of {c.Print()} = {list.IsSubset(d)}");
+
+            WriteLine(new string('═', 100));
 
             string[] str1 = { "one", "three", "five", "seven" };
             string[] str2 = { "one", "four" };
 
             SequentialList<string> glist = new SequentialList<string>(str1);
 
-            WriteLine($"{SequentialList<string>.Print(str2)} is subset of {SequentialList<string>.Print(str1)} = {glist.IsSubset(str2)}");
+            WriteLine($"{str2.Print()} is subset of {str1.Print()} = {glist.IsSubset(str2)}");
 
+            WriteLine(new string('═', 100));
         }
     }
 }
