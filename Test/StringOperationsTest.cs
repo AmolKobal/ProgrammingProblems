1e42b98b5773fd90bef12c4c2a94b96b87f25195
Amol Kobal <amol_kobal@rediffmail.com>
2019-11-05 21:58:39 +0530

    Different words, same letters Test Case

--- Test/StringOperationsTest.cs
+++ Test/StringOperationsTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using static System.Console;
 
 namespace ProgrammingProblems.Test
 {
@@ -10,6 +7,7 @@ namespace ProgrammingProblems.Test
         public static void Run()
         {
             WordsWithinWordsTest();
+            WordsOfSameLetterTest();
         }
 
         public static void WordsWithinWordsTest()
@@ -17,6 +15,13 @@ namespace ProgrammingProblems.Test
             string input = "There is a palm tree on the island.";
 
             StringOperations.WordsWithinWords(input);
+        }
+
+        public static void WordsOfSameLetterTest()
+        {
+            string input = "How can you listen if you are not silent ?";
+
+            StringOperations.WordsOfSameLetter(input);            
         }
 
     }
