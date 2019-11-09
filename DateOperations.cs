1e2a100bfdfdd4e60dc03f2667c2c11ed031490c
Amol Kobal <amol_kobal@rediffmail.com>
2019-11-09 19:54:26 +0530

    Date Operations

---
+++ DateOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgrammingProblems.Test
+{
+    class DateOperations
+    {
+
+        /*
+         * Write a program which will acctept a date in either long hand or short hand (eg. either 8/11/2019 or 8th November, 2019 ) and 
+         * work out if the given date is valid. eg. 14/13/2019 is clearly not valid.
+         */
+
+        public static bool IsValidDate(string input)
+        {
+            return true;
+        }
+
+
+        /*
+         * Time is often recorded within computers as something called a timestamp.Write a program which will accept two 
+         * timestamps and tell you the difference between them in human readable form.eg:
+         *
+         * Enter timestamps: 1573231345 1573236625	=> 1 hour 28 minutes
+         */
+        public static string TimestampDifference(long timestamp1, long timestamp2)
+        {
+            string output = "";
+
+            return output;
+        }
+
+    }
+}
